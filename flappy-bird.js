e820c1c95f97e50b6627c4b1b210a862837ff059
WilliamsChazal <w.chazal@codeur.online>
2021-04-25 16:00:01 +0200

    fonction saut

---
+++ flappy-bird.js
@@ -0,0 +1,38 @@
+const block = document.getElementById('block');
+const hole =document.getElementById('hole');
+const personnage =document.getElementById('personnage');
+const jumping = 0;
+
+/* hole.addEventListener('animationiteration', () => {
+    const random = -(( Math.random()*300)+150);
+    hole.style.top = random + "px";
+});  */ 
+hole.addEventListener('animationiteration', () => {
+    const random = Math.random()*3;
+    const top = (random*100)+150;
+    hole.style.top = -(top) + "px";
+});  
+
+setInterval(function(){
+    const personnageTop = parseInt(window.getComputedStyle(personnage).getPropertyValue("top"));
+    if(jumping==0){
+        personnage.style.top = (personnageTop+3)+"px";
+    }
+},10);
+
+function jump(){
+    jumping =1;
+    let jumpCount = 0;
+    const jumpInterval = setInterval(function(){
+        const personnageTop = parseInt(window.getComputedStyle(personnage).getPropertyValue("top"));
+        if((personnage>6)&&(counter<15)){
+             personnage.style.top = (personnageTop-5)+"px";
+         }
+        if( jumpCount>20){
+            clearInterval(jumpInterval);
+            jumping=0;
+            jumpCount=0;
+        }
+        jumpCount++;
+    },10);
+};
